e37507b4af4f9386e0e00f7da5493fa2a3caf802
Eden Le Comte <edenlc@gmail.com>
2017-07-01 14:41:06 +1200

    Converted all audio to MP3

--- www/js/audio.js
+++ www/js/audio.js
@@ -3,51 +3,51 @@ var audio = {};
 
 var audioInit = {
     twoforMe: function() {
-        var _audio = ['2/2.ogg', '2/2alt.ogg', '2/2alt1.ogg'];
+        var _audio = ['2/2.mp3', '2/2alt.mp3', '2/2alt1.mp3'];
         playEffect(_audio);
     },
     twoforTwo: function() {
-        var _audio = ['two4two.ogg'];
+        var _audio = ['two4two.mp3'];
         playEffect(_audio);
     },    
     threeIsRolled: function() {
-        var _audio = ['3/solo32.ogg', '3/solo33.ogg', '3/solo34.ogg', '3/solo35.ogg'];
+        var _audio = ['3/solo32.mp3', '3/solo33.mp3', '3/solo34.mp3', '3/solo35.mp3'];
         playEffect(_audio);
     }, 
     four: function() {
-        var _audio = ['4/4.ogg', '4/4alt.ogg'];
+        var _audio = ['4/4.mp3', '4/4alt.mp3'];
         playEffect(_audio);
     },       
     newThreeman: function() {
-        var _audio = ['new3Man.ogg', 'new3man1.ogg', 'new3man2.ogg', '3/3.ogg', '3/3alt1.ogg', '3/3alt3.ogg', '3/3alt5.ogg', '3/3alt7.ogg', '3/3alt8.ogg', '3/3alt9.ogg', '3/3alt10.ogg'];
+        var _audio = ['new3Man.mp3', 'new3man1.mp3', 'new3man2.mp3', '3/3.mp3', '3/3alt1.mp3', '3/3alt3.mp3', '3/3alt5.mp3', '3/3alt7.mp3', '3/3alt8.mp3', '3/3alt9.mp3', '3/3alt10.mp3'];
         playEffect(_audio);
     },        
     virginRoll: function() {
-        var _audio = ['Virgin.ogg', 'VirginRoll.ogg'];
+        var _audio = ['Virgin.mp3', 'VirginRoll.mp3'];
         playEffect(_audio);        
     },
     nextPlayer: function() {
-        var _audio = ['NextPlayer.ogg', 'NextPlayer1.ogg', 'NextPlayer2.ogg', '9/9alt.ogg'];
+        var _audio = ['NextPlayer.mp3', 'NextPlayer1.mp3', 'NextPlayer2.mp3', '9/9alt.mp3'];
         playEffect(_audio);        
     },    
     doubleThrees: function() {
-        var _audio = ['two3s.ogg', '6/6alt.ogg'];
+        var _audio = ['two3s.mp3', '6/6alt.mp3'];
         playEffect(_audio);                
     },
     seven: function() {
-        var _audio = ['7/7.ogg', '7/7alt.ogg', '7/7alt1.ogg', '7/7alt2.ogg'];
+        var _audio = ['7/7.mp3', '7/7alt.mp3', '7/7alt1.mp3', '7/7alt2.mp3'];
         playEffect(_audio);          
     },
     social: function() {
-        var _audio = ['10/10alt.ogg', '10/10alt1.ogg'];
+        var _audio = ['10/10alt.mp3', '10/10alt1.mp3'];
         playEffect(_audio);                  
     },
     eleven: function() {
-        var _audio = ['11/11.ogg', '11/11alt.ogg', '11/11alt1.ogg'];
+        var _audio = ['11/11.mp3', '11/11alt.mp3', '11/11alt1.mp3'];
         playEffect(_audio); 
     },
     doubleSix: function() {
-        var _audio = ['12/12.ogg', '12/12alt.ogg', '12/12alt1.ogg', '12/12alt2.ogg', '12/12alt3.ogg'];
+        var _audio = ['12/12.mp3', '12/12alt.mp3', '12/12alt1.mp3', '12/12alt2.mp3', '12/12alt3.mp3'];
         playEffect(_audio);         
     }
 };
@@ -67,7 +67,7 @@ audio["duelStart"] = new Audio();
 audio["duelStart"].src = "audio/its-time-to-duel.mp3";
 
 audio["welcome"] = new Audio();
-audio["welcome"].src = "audio/Welcometothreeman.ogg";
+audio["welcome"].src = "audio/Welcometothreeman.mp3";
 
 audio["mlghorn"] = new Audio();
 audio["mlghorn"].src = "audio/3airhorn.mp3";
